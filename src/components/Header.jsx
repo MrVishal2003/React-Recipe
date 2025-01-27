3117d90bb37aa169db5769a0f7d639058a3944c7
MrVishal2003 <hadiyal123vvv@gmail.com>
2025-01-27 15:38:49 +0530

    done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,19 @@
-import "./Header.css";
 import { GiSpoon } from "react-icons/gi";
 import { BiSolidUserCircle } from "react-icons/bi";
 import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div className="header-container">
-      <Link to={"/"}>
+    <div className="sticky top-0 z-50 bg-gray-900 text-white shadow-md flex items-center justify-between px-6 py-4">
+      <Link to="/" className="flex items-center text-2xl font-satisfy gap-2 text-white hover:text-amber-500">
         <h1>
           All Recipes
           <GiSpoon />
         </h1>
       </Link>
-      <BiSolidUserCircle className="user-img" />
+      <Link to="/profile" className="text-3xl text-white hover:text-amber-500 transition-transform duration-300 hover:scale-110">
+        <BiSolidUserCircle />
+      </Link>
     </div>
   );
 };
